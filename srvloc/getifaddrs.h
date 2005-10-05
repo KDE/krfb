48f165396a0ac9e33b893110b8ee71f9541da142
Dirk Mueller <mueller@kde.org>
2005-10-05 09:03:00 +0000

    SVN_SILENT: fix typo
    svn path=/trunk/KDE/kdenetwork/krfb/; revision=467420

--- srvloc/getifaddrs.h
+++ srvloc/getifaddrs.h
@@ -14,7 +14,7 @@
 
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
    02110-1301 USA.  */
 
 /**
