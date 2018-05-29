c4bc3ee3dfd687bdbec3eaceb8a46cba5741b50b
Oleg Chernovskiy <adonai@xaker.ru>
2018-05-30 00:00:42 +0300

    Fix IPv6 connectivity
    Summary:
    IPv6 is supported by LibVNCServer but for compatibility reasons its
    socket is backed by another file descriptor.
    
    This commit adds tracking IPv6 listen socket in the same way we now
    track IPv4 one.
    
    Differential Revision: https://phabricator.kde.org/D13209

--- krfb/rfbserver.cpp
+++ krfb/rfbserver.cpp
@@ -30,7 +30,8 @@ struct RfbServer::Private
     int listeningPort;
     bool passwordRequired;
     rfbScreenInfoPtr screen;
-    QSocketNotifier *notifier;
+    QSocketNotifier *ipv4notifier;
+    QSocketNotifier *ipv6notifier;
 };
 
 RfbServer::RfbServer(QObject *parent)
@@ -40,7 +41,8 @@ RfbServer::RfbServer(QObject *parent)
     d->listeningPort = 0;
     d->passwordRequired = true;
     d->screen = NULL;
-    d->notifier = NULL;
+    d->ipv4notifier = nullptr;
+    d->ipv6notifier = nullptr;
 
     RfbServerManager::instance()->registerServer(this);
 }
@@ -135,9 +137,13 @@ bool RfbServer::start()
         return false;
     };
 
-    d->notifier = new QSocketNotifier(d->screen->listenSock, QSocketNotifier::Read, this);
-    d->notifier->setEnabled(true);
-    connect(d->notifier, &QSocketNotifier::activated, this, &RfbServer::onListenSocketActivated);
+    d->ipv4notifier = new QSocketNotifier(d->screen->listenSock, QSocketNotifier::Read, this);
+    connect(d->ipv4notifier, &QSocketNotifier::activated, this, &RfbServer::onListenSocketActivated);
+    if (d->screen->listen6Sock > 0) {
+        // we're also listening on additional IPv6 socket, get events from there
+        d->ipv6notifier = new QSocketNotifier(d->screen->listen6Sock, QSocketNotifier::Read, this);
+        connect(d->ipv6notifier, &QSocketNotifier::activated, this, &RfbServer::onListenSocketActivated);
+    }
     connect(QApplication::clipboard(), &QClipboard::dataChanged,
             this, &RfbServer::krfbSendServerCutText);
 
@@ -148,10 +154,10 @@ void RfbServer::stop()
 {
     if (d->screen) {
         rfbShutdownServer(d->screen, true);
-        if (d->notifier) {
-            d->notifier->setEnabled(false);
-            d->notifier->deleteLater();
-            d->notifier = NULL;
+        for (auto notifier : {d->ipv4notifier, d->ipv6notifier})
+        if (notifier) {
+            notifier->setEnabled(false);
+            notifier->deleteLater();
         }
     }
 }
