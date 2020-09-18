8afd5c8df28f7c259337fd37a2e130b563419554
Aleix Pol <aleixpol@kde.org>
2020-09-18 18:03:24 +0200

    pipewire: Only aspire to use dmabuf if linux/dma-buf.h is present

--- framebuffers/pipewire/pw_framebuffer.cpp
+++ framebuffers/pipewire/pw_framebuffer.cpp
@@ -28,7 +28,9 @@
 
 #if PW_CHECK_VERSION(0, 2, 90)
 #include <spa/utils/result.h>
+#ifdef HAVE_LINUX_DMABUF_H
 #include <linux/dma-buf.h>
+#endif
 #include <sys/ioctl.h>
 #endif
 
@@ -690,7 +692,7 @@ void PWFrameBuffer::Private::onStreamProcess(void *data)
     pw_stream_queue_buffer(d->pwStream, buf);
 }
 
-#if PW_CHECK_VERSION(0, 2, 90)
+#if PW_CHECK_VERSION(0, 2, 90) && defined(HAVE_LINUX_DMABUF_H)
 static void syncDmaBuf(int fd, uint64_t start_or_end)
 {
     struct dma_buf_sync sync = { 0 };
@@ -731,6 +733,7 @@ void PWFrameBuffer::Private::handleFrame(pw_buffer *pwBuffer)
 
     std::function<void()> cleanup;
 #if PW_CHECK_VERSION(0, 2, 90)
+#ifdef HAVE_LINUX_DMABUF_H
     if (spaBuffer->datas->type == SPA_DATA_DmaBuf) {
         const int fd = spaBuffer->datas[0].fd;
         auto map = mmap(
@@ -748,7 +751,9 @@ void PWFrameBuffer::Private::handleFrame(pw_buffer *pwBuffer)
             syncDmaBuf(fd, DMA_BUF_SYNC_END);
             munmap(map, spaBuffer->datas[0].maxsize + spaBuffer->datas[0].mapoffset);
         };
-    } else if (spaBuffer->datas->type == SPA_DATA_MemFd) {
+    } else
+#endif
+    if (spaBuffer->datas->type == SPA_DATA_MemFd) {
         uint8_t *map = static_cast<uint8_t*>(mmap(
             nullptr, spaBuffer->datas->maxsize + spaBuffer->datas->mapoffset,
             PROT_READ, MAP_PRIVATE, spaBuffer->datas->fd, 0));
